35a40c76d436f0a0fbb8106e8aad145b7f82cca1
reginpv <reginpv@gmail.com>
2025-03-21 12:59:11 +0800

    feat: add demo how to fetch users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import TemplateDefault from "@/templates/Default"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/authOptions"
+import { getUsers } from "@/lib/actions/user"
+import Link from "next/link"
 
 export default async function Home() {
 
   const session = await getServerSession(authOptions)
+  const res = await getUsers()
+  const users = await res.payload
 
   return (
     <TemplateDefault>
@@ -23,6 +27,21 @@ export default async function Home() {
             <div>
               <p>This is a demo project built with Next.js 15, Neon PostgreSQL, Prisma, and Tailwind CSS v4, deployed on Vercel.</p>
             </div>
+
+            {
+              users && users.length > 0 && <div className="flex flex-col gap-2">
+                <h2>Users</h2>
+                <ul className="flex gap-3 flex-wrap">
+                  {
+                    users.map((user: User) => (
+                      <li key={user.id}>
+                        <Link href={`/user/${user.id}`} className="bg-gray-200 px-2 py-1 rounded-full">{user.name}</Link>
+                      </li>
+                    ))
+                  }
+                </ul>
+              </div>
+            }
 
           </div>
          
