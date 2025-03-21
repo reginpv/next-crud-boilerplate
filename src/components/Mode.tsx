36ac45d9146f33e8fa0237085b9e3e6e9246cb53
reginpv <reginpv@gmail.com>
2025-03-22 06:16:00 +0800

    feat: Icon component added

--- src/components/Mode.tsx
+++ src/components/Mode.tsx
@@ -1,14 +1,15 @@
 "use client"
 
 import { useMode } from "@/store/useMode"
+import { Icon } from "@/components/Icons"
 
 export default function Mode() {
 
   const { mode, toggleMode } = useMode()
 
   return (
-    <div>
-      <button onClick={()=>toggleMode()}>Mode {mode}</button>
-    </div>
+    <button onClick={()=>toggleMode()}>
+      <Icon icon="mode" className="dark:fill-white" />
+    </button>
   )
 }
