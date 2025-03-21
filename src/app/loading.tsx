8d5d57b63111d38dd1c560d1431611666c45f937
reginpv <reginpv@gmail.com>
2025-03-22 05:59:52 +0800

    update:loading.tsx styling

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -5,9 +5,9 @@ export default function Loading() {
       <section className="animate-pulse">
         <div className="container">
           <div className="flex flex-col gap-5">
-            <h1 className="w-[100px] bg-gray-200">&nbsp;</h1>
-            <p className="w-[140px] bg-gray-200">&nbsp;</p>
-            <p className="w-[120px] bg-gray-200">&nbsp;</p>
+            <h1 className="w-[100px] bg-gray-200 dark:bg-gray-900">&nbsp;</h1>
+            <p className="w-[140px] bg-gray-200 dark:bg-gray-900">&nbsp;</p>
+            <p className="w-[120px] bg-gray-200 dark:bg-gray-900">&nbsp;</p>
           </div>
         </div>
       </section>
